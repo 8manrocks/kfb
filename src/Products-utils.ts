219f6ae13e14e5dedb50bd2dedd5008abb2834f6
8man <8manrocks@gmail.com>
2023-01-07 23:07:11 +0530

    change img urls

--- src/Products-utils.ts
+++ src/Products-utils.ts
@@ -1,107 +1,107 @@
 export const products: Product[] = [{
     title: "Aaloo Bhujia",
-    img: "/Products/snack-01.png",
+    img: "/kfb/Products/snack-01.png",
     type: "Namkeen",
     desc: "Introducing KFB Aloo Bhujia - the ultimate snack for all your cravings! Our delicious aloo Bhujia is made from the finest ingredients and is packed with the perfect blend of spices. Whether you're looking for a quick snack on the go or a tasty addition to your meal, KFB Aloo Bhujia has got you covered. Not only is our aloo Bhujia delicious, it's also healthy! Made with real potatoes and a blend of natural spices, KFB Aloo Bhujia is a guilt-free snack that you can enjoy anytime, anywhere. So why wait? Try KFB Aloo Bhujia today and discover the ultimate snacking experience! Available now at a store near you."
 },
 {
     title: "Tikkha Mittha Mix",
-    img: "/Products/snack-05.png",
+    img: "/kfb/Products/snack-05.png",
     type: "Namkeen",
     desc: "Introducing KFB Tikkha Mittha Mix, the brand new flavour of Namkeen that's taking the snacking world by storm! With a perfect balance of spicy and sweet, this addictive snack is sure to become a household favourite. Made with a blend of aromatic spices and herbs, KFB Tikkha Mittha Mix Namkeen is the perfect snack for any occasion. Whether you're looking for a quick and satisfying snack on the go, or something to share with friends and family, this delicious Namkeen has you covered. So why wait? Try KFB Tikkha Mittha Mix Namkeen today and discover the delicious flavour that everyone is talking about! You won't be disappointed."
 },
 {
     title: "Pasta",
-    img: "/Products/snack-06.png",
+    img: "/kfb/Products/snack-06.png",
     type: "Fryums",
     desc: "Are you tired of the same old boring Fryums? Introducing KFB's new Pasta Fryums! These unique, crunchy Fryums are made from real pasta, giving them a delicious and satisfying bite. Perfect for dipping or snacking on their own, Pasta Fryums are the ultimate game changer in the chip aisle. Try them today and experience a whole new level of crunch!"
 },
 {
     title: "Tangy Tomato",
-    img: "/Products/snack-04.png",
+    img: "/kfb/Products/snack-04.png",
     type: "Wafers",
     desc: "Are you looking for a delicious and satisfying snack that's packed with bold flavor? Look no further than KFB's new Tangy Tomato Fryums! These Fryums are bursting with tangy goodness in every bite. They're perfect for satisfying your snack cravings, whether you're at home, at the office, or on the go. But don't just take our word for it – try KFB's Tangy Tomato Wafers for yourself and taste the difference. Trust us, you won't be disappointed!"
 },
 {
     title: "Sarvottam Besan",
-    img: "/Products/snack-02.png",
+    img: "/kfb/Products/snack-02.png",
     type: "Gram Flours",
     desc: "Are you looking for a nutritious and flavourful besan that is truly sarvottam? Look no further than Sarvottam Besan! This high-quality besan is made from the finest ingredients, ensuring that every bite is packed with nutrition and flavour. Whether you're using it for cooking or as a protein-rich addition to your diet, Sarvottam Besan is the perfect choice. So why wait? Try Sarvottam Besan today and experience the taste and nutrition that only the best can provide!"
 },
 {
     title: "Wheels",
-    img: "/Products/snack-03.png",
+    img: "/kfb/Products/snack-03.png",
     type: "Fryums",
     desc: "Are you looking for a new and exciting way to enjoy your favorite snacks? Look no further than KFB's crunchy new wheels Fryums! These Fryums are made from the freshest and most flavorful ingredients, resulting in a snack that is both satisfying and irresistible. Plus, with their unique wheel-shaped design, these Fryums are sure to be a hit at any party or gathering. So why wait? Order your bag of KFB's crunchy new wheels Fryums today and experience the ultimate snacking experience!"
 },
 {
     title: "Nylon Sev",
-    img: "/Products/snack-07.png",
+    img: "/kfb/Products/snack-07.png",
     type: "Namkeen",
     desc: "Are you looking for a unique and tasty snack that will satisfy your cravings? Look no further than nylon sev! This snack is made with high-quality ingredients and has a one-of-a-kind texture that is both crunchy and soft. Perfect for munching on while watching TV or as a topping on your favorite dishes, nylon sev is sure to become your new go-to snack. But don't just take our word for it – try nylon sev for yourself and see why it's quickly becoming a fan favorite."
 },
 {
     title: "Khatta Mittha",
-    img: "/Products/snack-08.png",
+    img: "/kfb/Products/snack-08.png",
     type: "Namkeen",
     desc: "Get ready for a taste explosion with Khatta Mittha Namkeen from KFB! This snack combines the traditional flavors of Namkeen with a zesty, khatta mittha twist that will tantalize your taste buds. Perfect for sharing with friends and family, or for snacking on the go. Try Khatta Mittha Namkeen today and discover a new twist on an old favorite. Available now at your local store or online. Don't miss out on this exciting new snack from KFB!"
 },
 {
     title: "Falari Chiwda",
-    img: "/Products/snack-09.png",
+    img: "/kfb/Products/snack-09.png",
     type: "Namkeen",
     desc: "Are you looking for a tasty and convenient snack to enjoy on-the-go? Introducing Falari Chiwda from KFB - the perfect combination of crunchy, savory, and spicy flavors that will satisfy your snack cravings! Made with the finest ingredients and expertly seasoned, Falari Chiwda is sure to become your go-to snack choice. Whether you're at work, at home, or on the road, Falari Chiwda is the perfect snack to keep you going. So why wait? Try Falari Chiwda from KFB today and discover a whole new world of snack-time delight!"
 },
 {
     title: "Classic Salted",
-    img: "/Products/snack-10.png",
+    img: "/kfb/Products/snack-10.png",
     type: "Wafers",
     desc: "Are you looking for a tasty and satisfying snack that will hit the spot every time? Look no further than classic salted Wafers from KFB! Our Wafers are made with only the highest quality ingredients, including the finest potatoes and a sprinkle of salt to bring out their natural flavor. Enjoy them on their own or pair them with your favorite dip for a delicious treat that's perfect for any occasion. Whether you're hosting a party, watching the game, or just craving a tasty snack, classic salted Wafers from KFB have got you covered."
 }]
 export const carouselProducts: Product[] = [
     {
         title: "Classic Salted",
-        img: "/Products/carousel-01.jpg",
+        img: "/kfb/Products/carousel-01.jpg",
         type: "Wafers"
     },
     {
-        img: "/Products/carousel-02.jpg",
+        img: "/kfb/Products/carousel-02.jpg",
         title: "Khatta Mittha",
         type: "Namkeen"
     },
     {
         title: "Nylon Sev",
-        img: "/Products/carousel-03.jpg",
+        img: "/kfb/Products/carousel-03.jpg",
         type: "Namkeen"
     },
     {
-        img: "/Products/carousel-04.jpg",
+        img: "/kfb/Products/carousel-04.jpg",
         title: "Wheels",
         type: "Fryums"
     },
     {
         title: "Aaloo Bhujia",
-        img: "/Products/carousel-05.jpg",
+        img: "/kfb/Products/carousel-05.jpg",
         type: "Namkeen"
     },
     {
         title: "Tikkha Mittha Mix",
-        img: "/Products/carousel-06.jpg",
+        img: "/kfb/Products/carousel-06.jpg",
         type: "Namkeen"
     },
     {
         title: "Tangy Tomato",
-        img: "/Products/carousel-07.jpg",
+        img: "/kfb/Products/carousel-07.jpg",
         type: "Wafers"
     },
     {
         title: "Pasta",
-        img: "/Products/carousel-08.jpg",
+        img: "/kfb/Products/carousel-08.jpg",
         type: "Fryums"
     },
     {
         title: "Falari Chiwda",
-        img: "/Products/carousel-09.jpg",
+        img: "/kfb/Products/carousel-09.jpg",
         type: "Namkeen"
     }
 ]
